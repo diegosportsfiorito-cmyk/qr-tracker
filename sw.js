afc8543ce7a2c8db7f986762406698aa63d2f5bb
diegosportsfiorito-cmyk <diego.sports.fiorito@gmail.com>
2026-05-28 00:45:16 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'qr-tracker-v1';
+const urlsToCache = [
+  '/qr-tracker/dashboard.html',
+  '/qr-tracker/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
